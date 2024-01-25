8d8f78e3b2f291aea2908e7c575e47841b5b1248
Jules Maestro <jlsodai@gmail.com>
2024-01-25 02:14:55 +0000

    add body color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className} text-gray-600`}>
         <AOSWrapper>{children}</AOSWrapper>
       </body>
     </html>
