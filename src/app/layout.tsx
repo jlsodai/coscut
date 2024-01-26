79f793c945418006a0af92ecedaeee2d93315f0c
Jules Maestro <jlsodai@gmail.com>
2024-01-26 16:19:06 +0000

    update font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Rubik } from "next/font/google";
 import "./globals.css";
 import AOSWrapper from "@/wrappers/AOSWrapper";
 
-const inter = Inter({ subsets: ["latin"] });
+const rubik = Rubik({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} text-gray-600`}>
+      <body className={`${rubik.className} text-gray-600`}>
         <AOSWrapper>{children}</AOSWrapper>
       </body>
     </html>
