b47b35f43f1e5c64f2ee4afb80ed86ac0124455e
Jules Maestro <jlsodai@gmail.com>
2024-01-27 02:12:40 +0000

    update meta description and title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,9 +6,9 @@ import AOSWrapper from "@/wrappers/AOSWrapper";
 const rubik = Rubik({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Coscut | Personalized Benefits for a Positive Workplace",
+  title: "Coscut - Personalized Benefits for a Positive Workplace",
   description:
-    "Coscut allows companies to revolutionize their approach to employee benefits, placing a focus on holistic well-being and satisfaction.",
+    "Coscut allows companies to boost their team's morale by revolutionizing their approach to employee benefits, placing a focus on holistic well-being and satisfaction.",
 };
 
 export default function RootLayout({
