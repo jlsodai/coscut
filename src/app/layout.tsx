b9876990512f6ba0a8c401021a3fa1572da8a145
Jules Maestro <jlsodai@gmail.com>
2024-01-27 02:29:55 +0000

    update open graph meta, remove static image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,10 @@ export const metadata: Metadata = {
   title: "Coscut - Personalized Benefits for a Positive Workplace",
   description:
     "Coscut allows companies to boost their team's morale by revolutionizing their approach to employee benefits, placing a focus on holistic well-being and satisfaction.",
+  openGraph: {
+    images:
+      "https://res.cloudinary.com/dmaestro/image/upload/v1706322512/open-graph_scwsjb.jpg",
+  },
 };
 
 export default function RootLayout({
