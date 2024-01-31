aa167fb457ebc88e30f7829e270ebeeb12f77fe8
Jules Maestro <jlsodai@gmail.com>
2024-01-31 04:24:33 +0000

    add negative screens

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,16 @@ const config = {
   ],
   prefix: "",
   theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "-sm": { max: "639px" },
+      "-md": { max: "767px" },
+      "-lg": { max: "1023px" },
+      "-xl": { max: "1279px" },
+    },
     container: {
       center: true,
       padding: "2rem",
