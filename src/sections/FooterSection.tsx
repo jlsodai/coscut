8cc74478fd97a040ab0ab64855be85bf7b6baefd
Jules Maestro <jlsodai@gmail.com>
2024-01-31 04:13:49 +0000

    change footer font size on mobile, add gap

--- src/sections/FooterSection.tsx
+++ src/sections/FooterSection.tsx
@@ -3,11 +3,11 @@ import React from "react";
 const FooterSection = () => {
   return (
     <section className="bg-cgreen-800 py-8">
-      <div className="container flex justify-between text-white items-center">
-        <h1 className="font-recoleta text-gray-50 text-5xl font-bold mb-2">
+      <div className="container flex justify-between text-white items-center gap-8">
+        <h1 className="font-recoleta text-gray-50 text-3xl md:text-5xl font-bold mb-2">
           coscut<span className="text-amber-500">.</span>
         </h1>
-        <p className="">© 2024 Coscut. All rights reserved</p>
+        <p className="text-right">© 2024 Coscut. All rights reserved</p>
       </div>
     </section>
   );
